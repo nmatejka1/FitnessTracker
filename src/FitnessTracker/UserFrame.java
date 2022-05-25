7229087d12ea0039c870f212a1ef12e50e52bc39
Nick Matejka <nmatejka@umd.edu>
2022-05-24 23:28:51 -0400

    rem

--- src/FitnessTracker/UserFrame.java
+++ src/FitnessTracker/UserFrame.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 
 public class UserFrame extends JFrame implements ActionListener {
     Container container = getContentPane();
-    private JLabel workoutsCompleted, caloriesProgress, proteinProgress;
-    private JButton workoutButton, dietButton, logoutButton;
+    private JLabel caloriesProgress, proteinProgress;
+    private JButton dietButton, logoutButton;
     private User user;
     private ArrayList<User> userList;
 
@@ -27,29 +27,24 @@ public class UserFrame extends JFrame implements ActionListener {
     }
  
     public void setInitialValues() {
-        workoutsCompleted = new JLabel("Exercises completed: ");
         caloriesProgress = new JLabel("Consumed: " + user.getTotalCalories() + "/" + user.getCalorieGoal() + " calories.");
         proteinProgress = new JLabel("Consumed: " + user.getTotalProtein() + "/" + user.getProteinGoal() + " protein.");
-        workoutButton = new JButton("Workouts");
-        workoutButton.addActionListener(this);
+       
         dietButton = new JButton("Diet");
-        dietButton.addActionListener(this);
         logoutButton = new JButton("Logout");
+
+        dietButton.addActionListener(this);
         logoutButton.addActionListener(this);
     }
  
     public void setLocationAndSize() {
-        workoutButton.setBounds(10, 20, 250, 25);
-        workoutsCompleted.setBounds(10, 50, 250, 25);
-        dietButton.setBounds(10, 80, 250, 25);
-        caloriesProgress.setBounds(10, 110, 250, 25);
-        proteinProgress.setBounds(10, 140, 250, 25);
+        dietButton.setBounds(10, 20, 250, 25);
+        caloriesProgress.setBounds(10, 50, 250, 25);
+        proteinProgress.setBounds(10, 80, 250, 25);
         logoutButton.setBounds(10, 170, 250, 25);
     }
  
-    public void addComponentsToContainer() {
-        container.add(workoutButton);
-        container.add(workoutsCompleted);
+    public void addComponentsToContainer() {;
         container.add(caloriesProgress);
         container.add(proteinProgress);
         container.add(dietButton);
@@ -62,11 +57,6 @@ public class UserFrame extends JFrame implements ActionListener {
         if (e.getSource() == logoutButton) {
             logoutExecute();
         }
-
-        else if (e.getSource() == workoutButton) {
-
-        }
-
         else {
             dietExecute();
         }
